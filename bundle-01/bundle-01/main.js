93c6d7ad03361be5ffe6f4c13b6854e6a09f46b6
juri931 <impa.mat@hotmail.it>
2024-03-08 15:30:10 +0100

    [add] es 2

--- bundle-01/bundle-01/main.js
+++ bundle-01/bundle-01/main.js
@@ -11,19 +11,22 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
 /*
 1. è un contatore
 2. non sono presenti errori di sintassi
 3. è presente l'errore logico in quanto i > 5 darà sempre una condizione falsa avendo dichiarato i = 0
 */  
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
 
 
 // ESERCIZIO 2
+/* funzione per controllare se un numero è pari oppure no, se vera, aggiunge +5 al numero.
+l'errore è nella condizione perché manca il doppio ==
+*/ 
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 == 0) {
         return num + 5;
     }
     return num;
@@ -31,22 +34,22 @@ function addIfEven(num) {
 
 
 // ESERCIZIO 3
-function loopToFive() {
-    for (let i = 0, i < 5, i++) {
-        console.log(i);
-    }
-}
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
-function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-    let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
-        }
-        return evenNumbers;
-    }
-}
-displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
